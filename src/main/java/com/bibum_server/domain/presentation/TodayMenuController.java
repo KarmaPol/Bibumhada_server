73477269816c04c50d946c7dbbe5250f4b4d92e5
hyunw9 <rkdgusdnr32@naver.com>
2023-09-10 21:27:00 +0900

    feat: exampleController

---
+++ src/main/java/com/bibum_server/domain/presentation/TodayMenuController.java
@@ -0,0 +1,17 @@
+package com.bibum_server.domain.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequiredArgsConstructor
+public class TodayMenuController {
+
+    @GetMapping(value = "/")
+    public String printHelloWorld(){
+        return "Hello world!";
+    }
+}
