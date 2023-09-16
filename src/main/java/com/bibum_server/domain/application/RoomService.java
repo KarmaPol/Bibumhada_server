75c362d539ff5b1ec6c036d71dc7f720cba7e379
hyunw9 <rkdgusdnr32@naver.com>
2023-09-16 15:45:03 +0900

    feat: voteService

---
+++ src/main/java/com/bibum_server/domain/application/RoomService.java
@@ -0,0 +1,67 @@
+package com.bibum_server.domain.application;
+
+import com.bibum_server.domain.dto.response.RoomDto;
+import com.bibum_server.domain.dto.request.LocationReq;
+import com.bibum_server.domain.dto.response.KakaoApiResponse;
+import com.bibum_server.domain.dto.response.RestaurantDto;
+import com.bibum_server.domain.restaurant.entity.Restaurant;
+import com.bibum_server.domain.restaurant.repository.RestaurantRepository;
+import com.bibum_server.domain.room.entity.Room;
+import com.bibum_server.domain.room.repository.RoomRepository;
+import com.bibum_server.domain.util.WebClientUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+public class RoomService {
+
+    private final RoomRepository roomRepository;
+    private final RestaurantRepository restaurantRepository;
+    private final WebClientUtil webClientUtil;
+
+    @Transactional
+    public RoomDto createRoom(LocationReq locationReq){
+        Room room = Room.builder()
+                .x(locationReq.getLongitude())
+                .y(locationReq.getLatitude())
+                .build();
+
+        roomRepository.save(room);
+
+        List<KakaoApiResponse.RestaurantResponse> restaurantResponses = webClientUtil.getRestaurant(locationReq);
+        List<Restaurant> restaurants = restaurantResponses.stream().map(restaurantResponse -> Restaurant.builder()
+                .title(restaurantResponse.getPlace_name())
+                .category(restaurantResponse.getCategory_name().substring(6).trim())
+                .link(restaurantResponse.getPlace_url())
+                .count(0L)
+                .distance(Long.valueOf(restaurantResponse.getDistance()))
+                .room(room)
+                .build()).collect(Collectors.toList());
+
+        room.addRestaurant(restaurants);
+
+        restaurantRepository.saveAll(restaurants);
+        List<RestaurantDto> RestaurantDtos = restaurants.stream().map(RestaurantDto::fromEntity).toList();
+
+        return RoomDto.builder()
+                .id(room.getId())
+                .x(room.getX())
+                .y(room.getY())
+                .restaurantDtoList(RestaurantDtos)
+                .build();
+
+
+    }
+    @Transactional
+    public RoomDto voteRestaurant(Long restaurantId){
+        Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(()-> new NoSuchElementException);
+        restaurant.
+    }
+}
---
+++ src/test/java/com/bibum_server/domain/presentation/TodayMenuControllerTest.java
@@ -0,0 +1,88 @@
+package com.bibum_server.domain.presentation;
+
+import com.bibum_server.domain.AbstractRestDocsTests;
+import com.bibum_server.domain.application.RoomService;
+import com.bibum_server.domain.dto.request.LocationReq;
+import com.bibum_server.domain.dto.response.RestaurantDto;
+import com.bibum_server.domain.dto.response.RoomDto;
+import com.bibum_server.domain.restaurant.entity.Restaurant;
+import com.bibum_server.domain.room.entity.Room;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.*;
+import java.util.stream.LongStream;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@WebMvcTest(TodayMenuController.class)
+class TodayMenuControllerTest extends AbstractRestDocsTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RoomService roomService;
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    @DisplayName("방을 생성한다.")
+    @Test
+    void createRoom() throws Exception {
+        LocationReq location = new LocationReq();
+        location.setLatitude("37.230840");
+        location.setLongitude("127.190607");
+        String locationRequest = mapper.writeValueAsString(location);
+
+        Room room = Room.builder()
+                .id(1L)
+                .x("1")
+                .y("2")
+                .build();
+
+        List<Restaurant> restaurantList = LongStream.range(0L, 3L)
+                .mapToObj((i) -> Restaurant.builder()
+                        .room(room)
+                        .id(i)
+                        .distance(1L)
+                        .count(0L)
+                        .link("www.test.com")
+                        .category("testCat")
+                        .title("test")
+                        .build()
+                ).toList();
+        room.addRestaurant(restaurantList);
+
+        List<RestaurantDto> restaurantDtoList = restaurantList.stream().map(RestaurantDto::fromEntity).toList();
+        RoomDto mockResponse = RoomDto.builder()
+                .id(room.getId())
+                .x(room.getX())
+                .y(room.getY())
+                .restaurantDtoList(restaurantDtoList)
+                .build();
+        given(roomService.createRoom(any(LocationReq.class))).willReturn(mockResponse);
+
+        this.mockMvc.perform(post("/create")  // 요청 URL 설정
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(locationRequest))
+                .andExpect(status().isOk())
+                .andDo(restDocs.document());
+    }
+
+
+    }
