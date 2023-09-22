2d0ebd65e00ee9a338a395c8ac8448e410c70688
hyunw9 <rkdgusdnr32@naver.com>
2023-09-22 14:51:39 +0900

    feat: webConfig

---
+++ src/main/java/com/bibum_server/global/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.bibum_server.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://43.201.167.198:8080",
+                        "http://localhost:3000",
+                        "https://localhost:3000",
+                        "https://todays-menu-front.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+        ;
+
+    }
+
+}
