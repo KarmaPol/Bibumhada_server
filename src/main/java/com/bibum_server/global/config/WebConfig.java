54b71d1954af7e5c551464c7b48b0f7e8ba2e02b
Younghwan Kim <88774925+royroyee@users.noreply.github.com>
2023-09-25 11:28:47 +0900

    Update WebConfig.java

--- src/main/java/com/bibum_server/global/config/WebConfig.java
+++ src/main/java/com/bibum_server/global/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://43.201.167.198:8080",
-                        "http://43.201.167.198",
+                .allowedOrigins("http://localhost:8080", 
+                        "http://todays-menu.kr,
                         "http://localhost:3000",
                         "https://localhost:3000",
                         "https://todays-menu-front.vercel.app",
