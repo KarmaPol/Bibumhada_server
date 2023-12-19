576ceef9647bc7d8ec036303e743f79921ac9c1b
KarmaPol <kkhkr98@gmail.com>
2023-12-20 01:39:04 +0900

    feat : 스웨거 url 추가

--- src/main/java/com/bibum_server/global/config/SwaggerConfig.java
+++ src/main/java/com/bibum_server/global/config/SwaggerConfig.java
@@ -2,12 +2,16 @@ package com.bibum_server.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(title = "오메 백엔드 API 명세서",
                 description = "비범하다 팀의 오메 백엔드 API 명세서 입니다",
-                version = "0.1"))
+                version = "0.1"),
+        servers = {
+        @Server(url = "/", description = "오메 서버 url")}
+)
 @Configuration
 public class SwaggerConfig {
 }
